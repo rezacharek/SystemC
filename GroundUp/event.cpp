3784c31c488f4a17607d8b89949b289f26b85e3b
Roman Zacharek <zacharekedward@gmail.com>
2020-06-18 13:42:39 +0200

    Fully functional event.cpp file

--- GroundUp/event.cpp
+++ GroundUp/event.cpp
@@ -1,10 +1,8 @@
 #include <systemc.h>
 
-SC_MODULE(missing_event)
+SC_MODULE(ordered_events)
 {
-    sc_event a_event;
-    
-    SC_CTOR(missing_event)
+    SC_CTOR(ordered_events)
     {
         SC_THREAD(B_thread);
         SC_THREAD(A_thread);
@@ -12,29 +10,50 @@ SC_MODULE(missing_event)
     }
 
     void A_thread()
-    {
-        a_event.notify();
-        std::cout << "A sent a_event!" << std::endl;
+    {   auto time = 0;
+        while(time < 200)
+        {
+            a_event.notify(SC_ZERO_TIME);
+            std::cout << "A sent a_event!" << std::endl;
+            wait(c_event);
+            std::cout << "A got c_event" << std::endl;
+            time += 100;
+        }
     }
 
     void B_thread()
     {
-        wait(a_event);
-        std::cout << "B got a_event!" << std::endl;
+        auto time = 0;
+        while(time < 200)
+        {
+            b_event.notify(SC_ZERO_TIME);
+            std::cout << "B sent b_event!" << std::endl;
+            wait(a_event);
+            std::cout << "B got a_event" << std::endl;
+            time += 100;
+        }
     }
 
     void C_thread()
     {
-        wait(a_event);
-        std::cout << "C got a_event!" << std::endl;
+        auto time = 0;
+        while(time < 200)
+        {
+            c_event.notify(SC_ZERO_TIME);
+            std::cout << "C sent c_event!" << std::endl;
+            wait(b_event);
+            cout << "C got b_event!" << std::endl;
+            time += 100;
+        }
     }
-
+    sc_event a_event, b_event, c_event;
 };
 
-int sc_main(int argc, char * argv [])
+int sc_main(int argc, char * argv[])
 {
-    missing_event ev("ev");
+    ordered_events events("events");
     sc_start();
-
+    wait(1,SC_MS);
+    sc_stop();
     return 0;
 }
